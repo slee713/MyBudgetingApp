66abf877bb39729f2ec56c43925bdac7dd32d35c
slee713 <steven.lee1393@gmail.com>
2020-09-08 09:29:13 -0400

    deleted unnecessary code

--- backend/app/controllers/transactions_controller.rb
+++ backend/app/controllers/transactions_controller.rb
@@ -1,35 +1,27 @@
 class TransactionsController < ApplicationController
     def index
-            user = User.find_by(username: params[:username])
-            if params[:year]
-                transactions = user.transactions.where("cast(strftime('%Y', date_of_transaction) as int) = #{params[:year]}")
-                if params[:month]
-                    month_transactions = transactions.where("cast(strftime('%m', date_of_transaction)as int) = #{params[:month]}")
-                    if params[:category] && params[:category]!= "All"
-                        category_transactions = month_transactions.where(category: params[:category])
-                        sorted_transactions = category_transactions.order("date_of_transaction ASC")
-                        render json: sorted_transactions
-                    else
-                        sorted_transactions = month_transactions.order("date_of_transaction ASC")
-                        render json: sorted_transactions
-                    end
-                else 
-                    sorted_transactions = transactions.order("date_of_transaction ASC")
+        user = User.find_by(username: params[:username])
+        if params[:year]
+            transactions = user.transactions.where("cast(strftime('%Y', date_of_transaction) as int) = #{params[:year]}")
+            if params[:month]
+                month_transactions = transactions.where("cast(strftime('%m', date_of_transaction)as int) = #{params[:month]}")
+                if params[:category] && params[:category]!= "All"
+                    category_transactions = month_transactions.where(category: params[:category])
+                    sorted_transactions = category_transactions.order("date_of_transaction ASC")
+                    render json: sorted_transactions
+                else
+                    sorted_transactions = month_transactions.order("date_of_transaction ASC")
                     render json: sorted_transactions
                 end
-            else
-                transactions = user.transactions.order("date_of_transaction ASC")
-                render json: transactions
+            else 
+                sorted_transactions = transactions.order("date_of_transaction ASC")
+                render json: sorted_transactions
             end
+        else
+            transactions = user.transactions.order("date_of_transaction ASC")
+            render json: transactions
+        end
     end
-
-    # def limit 
-    #     user = User.find_by(username: params[:username])
-    #     sort_transactions = user.transactions.order('date_of_transaction DESC')
-    #     # transactions = sort_transactions.paginate(page: params[:page], per_page: 20)
-        
-    #     render json: transactions
-    # end
 
     def create
         user = User.find_by(username: params[:username])
@@ -46,6 +38,7 @@ class TransactionsController < ApplicationController
 
     def update
         user = User.find_by(username: params[:username])
+    end
 
 
     private
