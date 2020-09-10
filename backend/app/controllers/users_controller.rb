4ef114d17704db43f81f2af82491aaba299345ad
slee713 <steven.lee1393@gmail.com>
2020-09-10 11:58:45 -0400

    fixed a couple of bugs with formatting

--- backend/app/controllers/users_controller.rb
+++ backend/app/controllers/users_controller.rb
@@ -3,13 +3,7 @@ class UsersController < ApplicationController
     
     def show
         user = User.find_by(username: params[:username])
-        if user 
-            render json: user
-        else
-            user = User.create(username: params[:username])
-            render json: user
-        end
-
+        render json: user
     end
 
     def update
--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   get '/users/:username', to: 'users#show'
   post '/users', to: 'users#create'
   patch '/users/:id', to: 'users#update'
-  delete '/users/:username', to: 'users#destroy'
+  delete '/users/:id', to: 'users#destroy'
 
   get '/transactions', to: 'transactions#index'
   get '/transactions/:username/', to: 'transactions#index'
