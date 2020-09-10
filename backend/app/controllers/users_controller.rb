f450a8a68d70707b28a25eb270630408d0112d01
slee713 <steven.lee1393@gmail.com>
2020-09-10 09:21:41 -0400

    added destroy to user

--- backend/app/controllers/users_controller.rb
+++ backend/app/controllers/users_controller.rb
@@ -17,6 +17,12 @@ class UsersController < ApplicationController
         user.update(user_params)
     end
 
+    def destroy
+        user = User.find(params[:id])
+        user.destroy()
+    end
+
+
 
     private
 
